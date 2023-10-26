e558ae9c1207937e764ca8f2aea9478103862190
Cas <cssamonte@gmail.com>
2023-10-25 18:37:29 -0700

    imported useState hook and created state declaration for variable gifs

--- react-giphy-searcher/src/components/App/index.jsx
+++ react-giphy-searcher/src/components/App/index.jsx
@@ -1,7 +1,9 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './styles.css'
 
 function App() {
+    // Store API data here
+    const [gifs, setGifs] = useState([])
 
   // Query the API on component mount
   useEffect(() => {
@@ -10,7 +12,7 @@ function App() {
     async function getData() {
       const res = await fetch('https://api.giphy.com/v1/gifs/trending/?api_key=xOtkJChTF94Y0SM4YPxhydzB9QSQevGM&limit=50')
       const { data } = await res.json()
-      console.log(data)
+      setGifs(data)
     }
 
     // Call the async function
@@ -22,6 +24,7 @@ function App() {
     <div>
       <h1>GifHub</h1>
       <h4>A React Giphy Searcher</h4>
+      <p>You have {gifs.length} gifs</p>
     </div>
   );
 }
