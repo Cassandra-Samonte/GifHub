0935270e7e575418d056d425248fecbf96abd0db
Cas <cssamonte@gmail.com>
2023-10-25 18:49:01 -0700

    displayed 1 gif :)

--- react-giphy-searcher/src/components/App/index.jsx
+++ react-giphy-searcher/src/components/App/index.jsx
@@ -13,6 +13,7 @@ function App() {
       const res = await fetch('https://api.giphy.com/v1/gifs/trending/?api_key=xOtkJChTF94Y0SM4YPxhydzB9QSQevGM&limit=50')
       const { data } = await res.json()
       setGifs(data)
+      console.log(data)
     }
 
     // Call the async function
@@ -25,6 +26,8 @@ function App() {
       <h1>GifHub</h1>
       <h4>A React Giphy Searcher</h4>
       <p>You have {gifs.length} gifs</p>
+      {gifs.length > 0 ? <img src={gifs[1].images.original.url} /> : <p>Your gif is loading...</p>}
+
     </div>
   );
 }
