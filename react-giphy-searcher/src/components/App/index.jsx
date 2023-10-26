1f6f42062267f9525dcedc435873e8ca208eda8f
Cas <cssamonte@gmail.com>
2023-10-25 18:29:42 -0700

    sent a query to the api and console.log the gif data

--- react-giphy-searcher/src/components/App/index.jsx
+++ react-giphy-searcher/src/components/App/index.jsx
@@ -1,8 +1,27 @@
+import { useEffect } from 'react'
+import './styles.css'
+
 function App() {
-  console.log(import.meta.env.VITE_GIPHY_KEY)
+
+  // Query the API on component mount
+  useEffect(() => {
+
+    // Define an async function to JSONify the query response  
+    async function getData() {
+      const res = await fetch('https://api.giphy.com/v1/gifs/trending/?api_key=xOtkJChTF94Y0SM4YPxhydzB9QSQevGM&limit=50')
+      const gif = await res.json()
+      console.log(gif)
+    }
+
+    // Call the async function
+    getData()
+  }, [])
+
+  //  Create the HTML using JSX for the App component
   return (
     <div>
-      <h1>React Giphy Searcher</h1>
+      <h1>GifHub</h1>
+      <h4>A React Giphy Searcher</h4>
     </div>
   );
 }
