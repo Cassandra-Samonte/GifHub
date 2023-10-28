8594335cd1880a013f9aa68fef39368734a022aa
Cas <cssamonte@gmail.com>
2023-10-28 15:00:15 -0700

    used ternary to for user detail display

--- react-giphy-searcher/src/components/Card/index.jsx
+++ react-giphy-searcher/src/components/Card/index.jsx
@@ -1,18 +1,21 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default function Card({ gifData, updateDetails }) {
     return (
         <Link
-            to={"/details"}
-            onClick={() => { updateDetails(gifData) }}
+            to="/details"
+            onClick={() => {
+                updateDetails(gifData);
+            }}
+            className="w-1/4 p-2" 
         >
-            <figure className="text-center m-2 border-2 border-black rounded-lg cursor-pointer bg-gray-700 bg-opacity-70 text-gray-300 hover:text-white hover:bg-gray-800 hover:transform hover:scale-105 shadow-lg transition ease duration-50">
-                <img className="w-full object-cover rounded" src={gifData.images.original.url} />
-                <figcaption>
-                    <h2 className="font-bold">{gifData.title}</h2>
-                    <h3 className="capitalize">{gifData.username}</h3>
-                </figcaption>
+            <figure className="cursor-pointer bg-gray-700 bg-opacity-70 text-gray-300 hover:text-white hover:bg-gray-800 hover:transform hover:scale-105 shadow-lg transition ease duration-50">
+                <img className="w-full h-56 object-cover" src={gifData.images.original.url} alt={gifData.title} /> 
+                {/* <figcaption className="p-2">
+                    <h2 className="font-bold text-center">{gifData.title}</h2>
+                    <h3 className="capitalize text-center">{gifData.username}</h3>
+                </figcaption> */}
             </figure>
         </Link>
-    )
+    );
 }
