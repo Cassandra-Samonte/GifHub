12c3923ad1274d6cb50f8cf128198f9406f75743
Cas <cssamonte@gmail.com>
2023-10-29 19:23:57 -0700

    styled details page

--- react-giphy-searcher/src/components/DetailsPage/index.jsx
+++ react-giphy-searcher/src/components/DetailsPage/index.jsx
@@ -1,22 +1,32 @@
+import './styles.css'
+
 export default function DetailsPage({ images, title, username, user }) {
-    console.log(user, 'is the user');
     return (
-        <div className="border border-white p-4">
-            <img src={images.original.url} alt="Image" />
-            <h1>{title}</h1>
-            {username && <h3>{username}</h3>}
-            { 
-              (user?.avatar_url || user?.profile_url) ? (
-                  <>
-                      {user?.avatar_url && <img src={user.avatar_url} alt="User Avatar" />}
-                      {user?.profile_url && <h3><a href={user.profile_url}>View Profile</a></h3>}
-                  </>
-              ) : (
-                  <p style={{color: 'red'}}>No avatar or profile associated with this image</p>
-              )
-            }
+        <div className="border border-white p-4 pt-16">
+            <div className="flex flex-col items-center">
+                <div className="grid grid-cols-2 gap-4 flex flex-col items-center">
+                    <img src={images.original.url} alt="Image" />
+        
+                    <div className="flex flex-col items-center">
+                        <h1 className="text-xl font-bold mb-2 text-center">{title}</h1>
+                        {username && <h3 className="text-lg mb-2 text-center">{username}</h3>}
+                        {user?.avatar_url && (
+                        <img
+                            src={user.avatar_url}
+                            alt="User Avatar"
+                            className="w-20 h-20 rounded-full mx-auto"
+                        />
+                        )}
+                        {user?.profile_url && (
+                        <p className="mt-2 text-center">
+                            <a href={user.profile_url} className="text-blue-500 hover:underline">
+                            View Profile
+                            </a>
+                        </p>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
-}
-
-
+  }
--- react-giphy-searcher/src/components/HomePage/index.jsx
+++ react-giphy-searcher/src/components/HomePage/index.jsx
@@ -5,8 +5,6 @@ import SearchPage from "../SearchPage"
 export default function HomePage(props) {
     return (
         <>
-            <h1>GifHub</h1>
-            <h4>A React Giphy Searcher</h4>
             {/* <SearchPage
             /> */}
             <Gallery 
