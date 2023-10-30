ac189682c3bdd15d60272436b3c796d13e452279
Cas <cssamonte@gmail.com>
2023-10-30 10:06:10 -0700

    details page css

--- react-giphy-searcher/src/components/Card/index.jsx
+++ react-giphy-searcher/src/components/Card/index.jsx
@@ -11,10 +11,6 @@ export default function Card({ gifData, updateDetails }) {
         >
             <figure className="cursor-pointer hover:text-white hover:transform hover:scale-105 shadow-lg transition ease duration-50">
             <img className="w-56 h-40 ml-1 mt-1 object-cover" src={gifData.images.original.url} alt={gifData.title} />
-                {/* <figcaption className="p-2">
-                    <h2 className="font-bold text-center">{gifData.title}</h2>
-                    <h3 className="capitalize text-center">{gifData.username}</h3>
-                </figcaption> */}
             </figure>
         </Link>
     );
--- react-giphy-searcher/src/components/DetailsPage/index.jsx
+++ react-giphy-searcher/src/components/DetailsPage/index.jsx
@@ -1,30 +1,35 @@
+import './styles.css'
+
 export default function DetailsPage({ images, title, username, user }) {
     return (
-        <div className="border border-white p-4 pt-16">
-            <div className="flex flex-col items-center">
-                <div className="grid grid-cols-2 gap-4 flex flex-col items-center">
-                    <img src={images.original.url} alt="Image" />
-        
-                    <div className="flex flex-col items-center">
-                        <h1 className="text-xl font-bold mb-2 text-center">{title}</h1>
-                        {username && <h3 className="text-lg mb-2 text-center">{username}</h3>}
-                        {user?.avatar_url && (
-                        <img
-                            src={user.avatar_url}
-                            alt="User Avatar"
-                            className="w-20 h-20 rounded-full mx-auto"
-                        />
-                        )}
-                        {user?.profile_url && (
-                        <p className="mt-2 text-center">
-                            <a href={user.profile_url} className="text-blue-500 hover:underline">
-                            View Profile
-                            </a>
-                        </p>
-                        )}
+        <div id="details">
+            <div className="border border-white p-4 pt-16">
+                <div className="flex flex-col items-center">
+                    <div className="grid grid-cols-2 gap-1 flex flex-col items-center">
+                        <img src={images.original.url} alt="Image" />
+            
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-xl font-bold mb-2 text-center">{title}</h1>
+                            {username && <h3 className="text-lg mb-2 text-center">{username}</h3>}
+                            {user?.avatar_url && (
+                            <img
+                                src={user.avatar_url}
+                                alt="User Avatar"
+                                className="w-20 h-20 rounded-full mx-auto"
+                            />
+                            )}
+                            {user?.profile_url && (
+                            <p className="mt-2 text-center">
+                                <a href={user.profile_url} className="text-blue-500 hover:underline">
+                                View Profile
+                                </a>
+                            </p>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+    
     );
   }
